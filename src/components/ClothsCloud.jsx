9cb58f80e6368a85d2b6be90a2377aeeb3d5a59f
QUDUSKUNLE <quduskunle@gmail.com>
2022-11-21 17:21:19 +0100

    Add develop

---
+++ src/components/ClothsCloud.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ClothsCloud() {
+  return (
+    <div>
+      <h1
+        style={{
+          color: 'green',
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}>Cloths Cloud</h1>
+    </div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClothsCloud from './components/ClothsCloud.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ClothsCloud />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
